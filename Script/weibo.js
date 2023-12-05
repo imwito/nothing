e67936b25d1ab851428758685cf0dee763ffc446
something <51877240+imwito@users.noreply.github.com>
2023-12-05 17:41:56 +0800

    Update weibo.js

--- Script/weibo.js
+++ Script/weibo.js
@@ -433,13 +433,13 @@ if (url.includes("/interface/sdk/sdkad.php")) {
           }
           newItems.push(item);
         } 
-        // else if (itemId === "100505_-_chaohua" || itemId === "100505_-_recentlyuser") {
-        //   newItems.push(item);
-        // }
-        else {
-          // 其他项目全部移除
-          continue;
+        else if (itemId === "100505_-_chaohua" || itemId === "100505_-_recentlyuser") {
+          newItems.push(item);
         }
+        // else {
+        //   // 其他项目全部移除
+        //   continue;
+        // }
       }
       obj.items = newItems;
     }
